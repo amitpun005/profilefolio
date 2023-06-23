2b3f1c86a753055c51f0292be9cdc09250829202
amitpun05 <amitpun05@gmail.com>
2023-06-23 23:09:41 +0530

    deleted unwanted files and images

--- js/myjs.js
+++ js/myjs.js
@@ -1,34 +1,37 @@
 function callTopArrow(section) {
-  alert("check" + section);
-  switch (section) {
-    case "skills":
-      document.getElementById("projects").style.position = "";
-      document.getElementById("skills").style.position = "relative";
-      document.getElementById("myBtn").style.position = "absolute";
+  if (section === "skills") {
+    alert("check" + section);
+    document.getElementById("projects").style.position = "";
+    document.getElementById("skills").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
 
-      let test = document.getElementById("myBtn");
-      test.style.top = "35rem";
-      test.style.right = "10px";
-      break;
-
-    case "projects":
-      document.getElementById("skills").style.position = "";
-      document.getElementById("projects").style.position = "relative";
-      document.getElementById("myBtn").style.position = "absolute";
-      let test2 = document.getElementById("myBtn");
-      test2.style.top = "37rem";
-      test2.style.right = "10px";
-      break;
-
-    case "default":
-      document.getElementById("projects").style.position = "";
-      document.getElementById("default-pos").style.position = "relative";
-      document.getElementById("myBtn").style.position = "absolute";
-      let defaultPosition = document.getElementById("myBtn");
-      defaultPosition.style.top = "-4.5rem";
-      defaultPosition.style.right = "0px";
-      break;
-    default:
-      break;
+    let test = document.getElementById("myBtn");
+    test.style.top = "35rem";
+    test.style.right = "10px";
+  } else if (section === "projects") {
+    alert("check" + section);
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+    let test2 = document.getElementById("myBtn");
+    test2.style.top = "38rem";
+    test2.style.right = "10px";
+  } else if (section === "defaults") {
+    alert("check" + section);
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "";
+    document.getElementById("myBtn").style.position = "";
+    let test3 = document.getElementById("myBtn");
+    test3.style.top = "";
+    test3.style.right = "10px";
+  } else {
+    alert("check" + section);
+    document.getElementById("projects").style.position = "";
+    document.getElementById("skills").style.position = "";
+    document.getElementById("default-pos").style.position = "";
+    document.getElementById("myBtn").style.position = "";
+    let defaultPosition = document.getElementById("myBtn");
+    defaultPosition.style.top = "0px";
+    defaultPosition.style.right = "0px";
   }
 }
