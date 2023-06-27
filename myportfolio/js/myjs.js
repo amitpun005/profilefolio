899a41989300e6793108e1e7699fe3de5d434546
amitpun005 <amitpun005@gmail.com>
2023-06-27 15:32:19 +0530

    all set

---
+++ myportfolio/js/myjs.js
@@ -0,0 +1,65 @@
+function callTopArrow(section) {
+  if (section === "skills") {
+    document.getElementById("projects").style.position = "";
+    document.getElementById("skills").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+
+    let test = document.getElementById("myBtn");
+    test.style.top = "35rem";
+    test.style.right = "1.5rem";
+  } else if (section === "projects") {
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+    let test2 = document.getElementById("myBtn");
+    test2.style.top = "38rem";
+    test2.style.right = "1.5rem";
+  }
+  else if (section === "edu") {
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "";
+    document.getElementById("myBtn").style.position = "";
+    document.getElementById("edu").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+    let test4 = document.getElementById("myBtn");
+    test4.style.top = "94rem";
+    test4.style.right = "1.5rem";
+  } 
+  else if (section === "edu") {
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "";
+    document.getElementById("edu").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+    let test4 = document.getElementById("myBtn");
+    test4.style.top = "94rem";
+    test4.style.right = "1.5rem";
+  } 
+  else if (section === "exp") {
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "";
+    document.getElementById("hobby").style.position = "";
+    document.getElementById("exp").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+    let test5 = document.getElementById("myBtn");
+    test5.style.top = "110rem";
+    test5.style.right = "1.5rem";
+  }
+  else if (section === "hobby") {
+    document.getElementById("skills").style.position = "";
+    document.getElementById("projects").style.position = "";
+    document.getElementById("exp").style.position = "";
+    document.getElementById("hobby").style.position = "relative";
+    document.getElementById("myBtn").style.position = "absolute";
+    let test5 = document.getElementById("myBtn");
+    test5.style.top = "120rem";
+    test5.style.right = "1.5rem";
+  } else {
+    document.getElementById("projects").style.position = "";
+    document.getElementById("skills").style.position = "";
+    document.getElementById("default-pos").style.position = "";
+    document.getElementById("myBtn").style.position = "";
+    let defaultPosition = document.getElementById("myBtn");
+    defaultPosition.style.top = "0px";
+    defaultPosition.style.right = "0px";
+  }
+}
