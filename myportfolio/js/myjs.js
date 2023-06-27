61bc9f66f4bd65f1bc923413cecb2270acc0beb8
amitpun005 <amitpun005@gmail.com>
2023-06-27 22:24:37 +0530

    code updated

--- myportfolio/js/myjs.js
+++ myportfolio/js/myjs.js
@@ -1,6 +1,9 @@
 function callTopArrow(section) {
   if (section === "skills") {
     document.getElementById("projects").style.position = "";
+    document.getElementById("hobby").style.position = "";
+    document.getElementById("exp").style.position = "";
+    document.getElementById("edu").style.position = "";
     document.getElementById("skills").style.position = "relative";
     document.getElementById("myBtn").style.position = "absolute";
 
@@ -9,6 +12,9 @@ function callTopArrow(section) {
     test.style.right = "1.5rem";
   } else if (section === "projects") {
     document.getElementById("skills").style.position = "";
+    document.getElementById("hobby").style.position = "";
+    document.getElementById("exp").style.position = "";
+    document.getElementById("edu").style.position = "";
     document.getElementById("projects").style.position = "relative";
     document.getElementById("myBtn").style.position = "absolute";
     let test2 = document.getElementById("myBtn");
@@ -19,40 +25,35 @@ function callTopArrow(section) {
     document.getElementById("skills").style.position = "";
     document.getElementById("projects").style.position = "";
     document.getElementById("myBtn").style.position = "";
+    document.getElementById("hobby").style.position = "";
     document.getElementById("edu").style.position = "relative";
     document.getElementById("myBtn").style.position = "absolute";
-    let test4 = document.getElementById("myBtn");
-    test4.style.top = "94rem";
-    test4.style.right = "1.5rem";
+    let test3 = document.getElementById("myBtn");
+    test3.style.top = "96rem";
+    test3.style.right = "1.5rem";
   } 
-  else if (section === "edu") {
-    document.getElementById("skills").style.position = "";
-    document.getElementById("projects").style.position = "";
-    document.getElementById("edu").style.position = "relative";
-    document.getElementById("myBtn").style.position = "absolute";
-    let test4 = document.getElementById("myBtn");
-    test4.style.top = "94rem";
-    test4.style.right = "1.5rem";
-  } 
+ 
   else if (section === "exp") {
     document.getElementById("skills").style.position = "";
     document.getElementById("projects").style.position = "";
     document.getElementById("hobby").style.position = "";
+    document.getElementById("edu").style.position = "";
     document.getElementById("exp").style.position = "relative";
     document.getElementById("myBtn").style.position = "absolute";
     let test5 = document.getElementById("myBtn");
-    test5.style.top = "110rem";
+    test5.style.top = "108rem";
     test5.style.right = "1.5rem";
   }
   else if (section === "hobby") {
     document.getElementById("skills").style.position = "";
     document.getElementById("projects").style.position = "";
     document.getElementById("exp").style.position = "";
+    document.getElementById("edu").style.position = "";
     document.getElementById("hobby").style.position = "relative";
     document.getElementById("myBtn").style.position = "absolute";
-    let test5 = document.getElementById("myBtn");
-    test5.style.top = "120rem";
-    test5.style.right = "1.5rem";
+    let test6 = document.getElementById("myBtn");
+    test6.style.top = "124rem";
+    test6.style.right = "1.5rem";
   } else {
     document.getElementById("projects").style.position = "";
     document.getElementById("skills").style.position = "";
